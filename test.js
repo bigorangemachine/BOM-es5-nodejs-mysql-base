62ae851d68552b77569e5355636e46fb289b972a
Brian Dinga <brian.dinga@telus.com>
2016-09-18 10:01:37 -0400

    Adjusted test.js sample

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 //modules
 var _ = require('underscore'),//http://underscorejs.org/
     merge = require('merge'),//allows deep merge of objects
-    mysql = require('mysql'),//https://github.com/felixge/node-mysql/
     fs = require('fs'),
     http = require('http'),
     url = require('url'),
@@ -9,89 +8,70 @@ var _ = require('underscore'),//http://underscorejs.org/
     md5 = require('md5'),
     repeat = require('string.prototype.repeat'),//FOR EASY DEBUGGING :D
     JSON = require('JSON'),
-    utils=require('bom-utils'),
-    vars=require('bom-utils/vars');
-//custom modules
-// var utils = require('./jspkg/utils'),vars = require('./jspkg/vars');
+    utils = require('bom-utils'),
+    vars = require('bom-utils/vars');
+//custom modules - for WIP
+// var utils = require('./jspkg/utils'),
+//     vars = require('./jspkg/vars');
 //varaibles
 var doc_root='',
     root_params={
         'silent':false,//actual settings
+        'rootmodule':'',
         'config':'./config',
         'found_params':[]
     };
-
-var config=require('./jspkg/configurator')(process, fs, root_params);
+root_params.config=root_params.config+'.test';
+var config=require('../configurator')(process, fs, root_params);
 doc_root=root_params.doc_root;
 
-if(!config || config.db.type.toLowerCase()!=='mysql'){console.error('ONLY DEVELOPED FOR MYSQL');process.exit();}//my apps prefer mysql
-if(!root_params.silent){console.log('DB SETTINGS: ',merge(true,{},config.db,{'user':vars.const_str.omitted,'pass':vars.const_str.omitted}));}
-var mysql_conn = mysql.createConnection({
-        //'debug':true,
-        'database':config.db.db,
-        'host': config.db.host,
-        'user': config.db.user,
-        'password': config.db.pass
-    });
-mysql_conn.version=config.db.version;
 
-var terminate_manifest=[], // {'obj_this': obj, 'func': 'func_name', 'args': [] } -> might need , 'sync_next': function(){}, 'next_arg': [null, null,{'next':true}, null]
-    do_terminate=function(reportTrace){
-        if(terminate_manifest.length>0){
-            for(var t=0;t<terminate_manifest.length;t++){
-                var task=terminate_manifest[t],
-                    args=(typeof(task.args)==='object' && task.args.length>0?task.args:[]),
-                    new_func=false,
-                    valid_obj_prop=(utils.obj_valid_key(task,'obj_this') && typeof(task.obj_this)==='object'?true:false);
-
-                if(valid_obj_prop && utils.obj_valid_key(task,'func') && typeof(task.func)==='string' && typeof(task.obj_this[task.func])==='function'){
-                    new_func=task.obj_this[task.func].bind(task.obj_this);
-                }else if(valid_obj_prop && (utils.obj_valid_key(task,'func') && typeof(task.func)==='function')){
-                    new_func=task.func.bind(task.obj_this);
-                }else if(typeof(task.func)==='function'){
-                    new_func=task.func.bind(null);
-                }
-                if(new_func!==false){
-                    new_func.apply(null,args);
-                }
-            }
-        }
+var do_terminate=function(reportTrace){
         if(!root_params.silent){
             if(reportTrace){console.trace();}
             console.log("\n\n\n================= do_terminate PID: "+process.pid+" =================","\n");
         }
-		mysql_conn.end(function(err){/*The connection is terminated now*/
 //console.log('===mysql_conn.end===',arguments);
-            process.on('exit', function(code){
-                if(!root_params.silent){
-                    console.log('===PROCESS process.on(\'exit\') EVENT===');
-                    console.log("\n================= \\\\do_terminate PID: "+process.pid+" =================","\n\n");
-                }
-            });
-            process.exit();//nothing happens after this - except the event hadler
-		});
+        process.on('exit', function(code){
+            if(!root_params.silent){
+                console.log('===PROCESS process.on(\'exit\') EVENT===');
+                console.log("\n================= \\\\do_terminate PID: "+process.pid+" =================","\n\n");
+            }
+        });
+        process.exit();//nothing happens after this - except the event hadler
     },
     do_init=function(){//initalize
-        /*
-            - start mysql
-        */
-		mysql_conn.connect(function(err) {
-			if(err){
-                console.error('MYSQL ERROR CONNECTING: ' + err.toString());//was err.stack - more detailed
-                do_terminate(false);
-                return;
-            }
+        //custom modules
+        var subTests=require('./sub/tests')(),
+            theModel=require('./sub/theModel')();
 
-            //custom modules -  mysql dependent
-            var genericDB=require('./jspkg/genericDB')(mysql_conn),
-                logicBase=require('./jspkg/genDB/logic')(),
-                logDB=require('./jspkg/logDB'),
-                logDB_obj=new logDB(),
-                logicBase_obj=new logicBase(),
-                genericDB_obj=new genericDB();
-console.log("DO YOUR TESTS!");
-            do_terminate(false);
-        });
+        var do_sets=[
+            function(doNext){
+                /*
+                * !!!!!!!!!!!!!!!!!!!!!!!!!! TESTING THESE!!!!
+                * theModel(doFunc)
+                * theModel(doFunc,opts)
+                * theModel(posFunc,negFunc,doFunc,opts)
+                * theModel(posFunc,negFunc,doFunc,idleFunc,opts)
+                */
+                try{test1=new theModel(function(){});}
+                catch(e){do_err("[theModel TEST] Could not build 'SINGLE ARG'\n"+e.toString());}
+
+                doNext();
+            },
+            function(doNext){
+                subTests.typical(doNext);
+            }
+        ];
+        var binded_dos=[];
+        for(var d=0;d<do_sets.length;d++){
+            binded_dos.push((function(index){
+                return function(){
+                    do_sets[index]((binded_dos.length-1>index && typeof(binded_dos[index+1])==='function'?binded_dos[index+1]:do_terminate));//do_terminate(false);
+                };
+            })(d));
+        }
+        binded_dos[0]();
 	};
 
 do_init();
